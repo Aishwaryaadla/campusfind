32b5a25fea52cb789226f9ecc2f0f1cd84ec8a7d
Aishwarya <aishwaryaadla007@gmail.com>
2025-07-19 13:31:57 +0530

    updated port

--- server/app.js
+++ server/app.js
@@ -8,14 +8,15 @@ import founditemsroute from './routes/founditems.js';
 const app = express();
 
 dotenv.config();
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.use("/api/lostitems",lostitemsroute);
 app.use("/api/founditems",founditemsroute);
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:4000");
+    console.log("Server started at http://localhost:"+PORT);
 });
 
