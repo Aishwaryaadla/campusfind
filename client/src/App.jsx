86c3cf2e049f1d4ae6eb4105c95e2d4a67ff51a3
Aishwarya <aishwaryaadla007@gmail.com>
2025-07-20 16:47:41 +0530

    setup daisy ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,14 @@ import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>
+    <div data-theme="cupcake">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
       <Routes>
         <Route path = '/' element={<HomePage/>}/>
         <Route path = '/lost' element={<LostPage/>}/>
