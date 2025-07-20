601c340ab6c2012b068cf03931d33056489dc536
Aishwarya <aishwaryaadla007@gmail.com>
2025-07-20 16:30:42 +0530

    installed tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Route,Routes } from 'react-router';
+import AboutPage from './pages/AboutPage';
+import FoundPage from './pages/FoundPage';
+import HomePage from './pages/HomePage';
+import LostPage from './pages/LostPage';
+import toast from "react-hot-toast";
+
 
 const App = () => {
   return (
     <div>
-      App
+      <Routes>
+        <Route path = '/' element={<HomePage/>}/>
+        <Route path = '/lost' element={<LostPage/>}/>
+        <Route path = '/found' element={<FoundPage/>}/>
+        <Route path = '/about' element={<AboutPage/>}/>
+      </Routes>
     </div>
   )
 }
